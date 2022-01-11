dbe599bf3ca8868ad3c03cbc54a1058928387ae5
Dmitrii Boykov <boikovdmitriy@mail.ru>
2022-01-11 23:26:27 +0300

    [ADD] added auth0

--- src/index.tsx
+++ src/index.tsx
@@ -1,28 +1,39 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Auth0Provider } from "@auth0/auth0-react";
+import CssBaseline from "@mui/material/CssBaseline";
 
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 import { Home } from "./pages/Home";
 import { Dashboard } from "./pages/Dashboard";
 
+const providerConfig = {
+  domain: process.env.REACT_APP_AUTH0_DOMAIN || "",
+  clientId: process.env.REACT_APP_AUTH0_CLIENT_ID || "",
+  audience: process.env.REACT_APP_AUTH0_AUDIENCE || "",
+};
+
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route
-          path="*"
-          element={
-            <main style={{ padding: "1rem" }}>
-              <p>not found</p>
-            </main>
-          }
-        />
-      </Routes>
-    </BrowserRouter>
+    <CssBaseline />
+    <Auth0Provider {...providerConfig} redirectUri={window.location.origin}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route
+            path="*"
+            element={
+              <main style={{ padding: "1rem" }}>
+                <p>not found</p>
+              </main>
+            }
+          />
+        </Routes>
+      </BrowserRouter>
+    </Auth0Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,3 +1,31 @@
+import { useAuth0 } from "@auth0/auth0-react";
+import { Box, Button, Container, Link } from "@mui/material";
+import { NavLink } from "react-router-dom";
+import { useCallback } from "react";
+
 export const Home = () => {
-  return <div>home page</div>;
+  const { isAuthenticated, loginWithRedirect, isLoading, logout } = useAuth0();
+
+  const onAuthClick = useCallback(() => {
+    if (isAuthenticated) {
+      logout();
+      return;
+    }
+    loginWithRedirect();
+  }, [isAuthenticated, loginWithRedirect, logout]);
+
+  return (
+    <Container fixed>
+      <Box sx={{ bgcolor: "#cfe8fc", height: "100vh" }}>
+        {isAuthenticated && (
+          <Link component={NavLink} to="/dashboard">
+            Dashboard
+          </Link>
+        )}
+        <Button disabled={isLoading} variant="contained" onClick={onAuthClick}>
+          {isAuthenticated ? "logout" : "Log In"}
+        </Button>
+      </Box>
+    </Container>
+  );
 };
